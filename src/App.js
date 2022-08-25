1e4439b742691b5623075c32df8132829dbfc52e
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-26 01:07:25 +0530

    init shopping cart setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import './App.css';
-import ToolTip from './components/tool-tip/ToolTip';
 
 function App() {
-  return (
-    <div className='App'>
-      <ToolTip element={<Box />} message='Hi my name is so and so' />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
-
-function Box() {
-  return (
-    <div
-      style={{ width: '500px', height: '400px', backgroundColor: 'lightblue' }}
-    ></div>
-  );
-}
