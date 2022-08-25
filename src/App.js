5c5e58ecabdf74e5be2eca53e0f988df945fcb84
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-26 03:10:12 +0530

    reset main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import './App.css';
-import React, { useState, createContext } from 'react';
-import DummyComponent from './components/ctx-component/DummyComponent';
-import DummyComponent2 from './components/ctx-component/DummyComponent2';
-
-export const ThemeContext = createContext();
-
 function App() {
-  const [dark, setDark] = useState(true);
-
-  return (
-    <ThemeContext.Provider value={{ dark, setDark }}>
-      <div className=''>
-        <DummyComponent />
-        <DummyComponent2 />
-      </div>
-    </ThemeContext.Provider>
-  );
+  return <div className=''>Hello world</div>;
 }
 
 export default App;
